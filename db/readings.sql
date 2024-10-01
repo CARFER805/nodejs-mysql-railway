ded5cee4364076e2eedb5bd01014f4f833043fad
Fernando Cardozo <carfer805@gmail.com>
2024-10-01 15:07:57 -0300

    Mi primer commit

---
+++ db/readings.sql
@@ -0,0 +1,7 @@
+CREATE TABLE readings (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    SensorId INT,
+    Timestamp DATE,
+    Value REAL
+   
+);
---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import { pool } from './db.js';
+import { PORT } from './config.js';
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM readings');
+
+  res.json(rows);
+});
+app.get('/ping', async (req, res) => {
+  const [result] = await pool.query(`SELECT "hello world" as RESULT`);
+
+  res.json(result[0]);
+});
+
+app.get('/create', async (req, res) => {
+  try {
+    const result = await pool.query(
+      'INSERT INTO readings (SensorId, Timestamp, Value) VALUES ' +
+        "(1, '2024-10-01 10:00:00', 23.5), " +
+        "(2, '2024-10-01 10:05:00', 45.2), " +
+        "(3, '2024-10-01 10:10:00', 12.3)"
+    );
+
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error inserting data' });
+  }
+});
+
+app.listen(PORT);
+console.log('Server on port', PORT);
