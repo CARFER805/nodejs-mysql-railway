a2e46ddffe395aba43e491f89a7dc063a268791a
Fernando Cardozo <carfer805@gmail.com>
2024-10-02 23:03:33 -0300

    Cambio en el archivo config.js en la variable DB_HOST

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_HOST = process.env.DB_HOST || 'mysql.railway.internal';
 export const DB_USER = process.env.DB_USER || 'arduino';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'sensores';
 export const DB_NAME = process.env.DB_NAME || 'arduinoDB';
