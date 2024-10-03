e805cfbe4bfda859f07c6d99f22f4b2138a63797
Fernando Cardozo <carfer805@gmail.com>
2024-10-02 23:24:50 -0300

    Cambio en el archivo config.js de todas las variables de conexion de base de datos

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || 'mysql.railway.internal';
-export const DB_USER = process.env.DB_USER || 'arduino';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'sensores';
-export const DB_NAME = process.env.DB_NAME || 'arduinoDB';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD =
+  process.env.DB_PASSWORD || 'swKZkXrFownVuboeRAyoiIKvfFljzlGNW';
+export const DB_NAME = process.env.DB_NAME || 'railway';
 export const DB_PORT = process.env.DB_PORT || '3306';
