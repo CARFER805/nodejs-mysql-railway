82d3226eb1c9f78d56f4abeb64d5b448d6b5ee79
Fernando Cardozo <carfer805@gmail.com>
2024-10-02 23:51:43 -0300

    Cambio en el archivo config.js de todas las variables de conexion de base de datos segun RailWay

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'mysql.railway.internal';
-export const DB_USER = process.env.DB_USER || 'root';
+export const DB_HOST = process.env.MYSQLHOST || MYSQLHOST; // Cambia a MYSQLHOST
+export const DB_USER = process.env.MYSQLUSER || MYSQLUSER; // Cambia a MYSQLUSER
 export const DB_PASSWORD =
-  process.env.DB_PASSWORD || 'swKZkXrFownVuboeRAyoiIKvfFljzlGNW';
-export const DB_NAME = process.env.DB_NAME || 'railway';
-export const DB_PORT = process.env.DB_PORT || '3306';
+  process.env.MYSQLPASSWORD || 'wKZkXrFownVuboeRAyoiIKvfFljzlGNW'; // Asegúrate que esta contraseña sea correcta
+export const DB_NAME = process.env.MYSQLDATABASE || MYSQLDATABASE; // Cambia a MYSQLDATABASE
+export const DB_PORT = process.env.MYSQLPORT || MYSQLPORT; // Cambia a MYSQLPORT
