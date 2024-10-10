9a5c93d6c0eeca6b46a1ec001cebf3216b71de52
Fernando Cardozo <carfer805@gmail.com>
2024-10-10 15:32:13 -0300

    Correccion de Errores en el archivo config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || '34,66,129,245';
+export const DB_HOST = process.env.DB_HOST || '34.66.129.245';
 export const DB_USER = process.env.DB_USER || 'arduino';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'carfer805';
 export const DB_NAME = process.env.DB_NAME || 'arduinoDB';
