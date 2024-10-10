795f41b6b82d92f5c222ec19717cb6009f8200ed
Fernando Cardozo <carfer805@gmail.com>
2024-10-10 15:18:35 -0300

    Cambio de las credencuales de Base de Datos de Railway a Google Cloud

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || 'junction.proxy.rlwy.net';
-export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD =
-  process.env.DB_PASSWORD || 'wKZkXrFownVuboeRAyoiIKvfFljzlGNW';
-export const DB_NAME = process.env.DB_NAME || 'railway';
-export const DB_PORT = process.env.DB_PORT || '48974';
+export const DB_HOST = process.env.DB_HOST || '34,66,129,245';
+export const DB_USER = process.env.DB_USER || 'arduino';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'carfer805';
+export const DB_NAME = process.env.DB_NAME || 'arduinoDB';
+export const DB_PORT = process.env.DB_PORT || '3306';
